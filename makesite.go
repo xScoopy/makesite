122d0c98a4ec956ad9e2e58999f197d8821639d6
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-01-30 15:32:26 -0600

    added base elements to template, finished steps 1 and 2 on tasklist, created structs in preparation for step 3

--- makesite.go
+++ makesite.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+type Header struct {
+	Content string
+}
+type Paragraph struct {
+	Section []string
+}
+// Read in the contents of the provided first-post.txt file
+func readFile() string {
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	return(string(fileContents))
+}
+//Edit the provided HTML template (`template.tmpl`)
+//to display the contents of `first-post.txt`.
+func updateTemplate(fileContents string) {
 
+}
 func main() {
 	fmt.Println("Hello, world!")
+	fmt.Print(readFile())
 }
