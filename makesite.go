7af956892c968663319a1809fc0585200ffcd893
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-06 19:33:03 -0600

    able to print out all .txt extensions in current directory

--- makesite.go
+++ makesite.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 type Content struct {
@@ -28,10 +30,22 @@ func savePage(newFileName string) {
 }
 
 func main() {
+	dirFlag := flag.String("dir", "..", "Directory to parse for txt files")
+
 	//use flag to get input data
 	fileToRead := flag.String("file", "first-post.txt", "File to parse to html")
 	flag.Parse()
-    
+	//output all txt files in current directory
+	directory := *dirFlag
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".txt" {
+			fmt.Printf("%v\n", file.Name())
+		}
+	}
 	//generate output file name
 	outputFileMinusExt := strings.Split(*fileToRead, ".")[0]
 	outputFile := outputFileMinusExt + ".html"
